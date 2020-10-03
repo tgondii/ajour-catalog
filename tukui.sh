9259bc9a3d31274032832bc51c6ee7988b64ee6b
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2020-10-03 17:46:40 +0200

    chore: tukui ids to int

--- tukui.sh
+++ tukui.sh
@@ -6,4 +6,4 @@ if [ $# -eq 0 ]
 fi
 tukui_endpoint="https://www.tukui.org/api.php?addons=all"
 data=$(curl -s $tukui_endpoint)
-echo "$data" | jq -c 'map({"id": .id, "name": .name, "summary": .small_desc | gsub("[\\r\\n\\t]"; ""), "numberOfDownloads": .downloads | tonumber, "categories": [.category], "source": "tukui" })' > $1
+echo "$data" | jq -c 'map({"id": .id | tonumber, "name": .name, "summary": .small_desc | gsub("[\\r\\n\\t]"; ""), "numberOfDownloads": .downloads | tonumber, "categories": [.category], "source": "tukui" })' > $1
