1029a79bd0368a475098fe3f7e22d0900f3233ee
Casper Storm <casper+github@rogildstorm.com>
2020-10-03 11:08:48 +0200

    chore: initial bash work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+index=0
+page_size=5
+number_of_addons=0
+curse_endpoint="https://addons-ecs.forgesvc.net/api/v2/addon/search?gameId=1&pageSize=${page_size}"
+unset addons
+
+while [ $number_of_addons -le $page_size ]
+do
+  data=$(curl -s curse_endpoint)
+
+  # number_of_addons = data.length
+  # concat addons.
+
+  sleep 1
+  index=$(( $index + $page_size ))
+done
+
+echo "::set-output data=addons::$addons"
