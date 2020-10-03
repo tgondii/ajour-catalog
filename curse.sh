7da4806248ca3503f77c757f8b62f6dc7fe42e4a
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2020-10-03 17:17:50 +0200

    chore: added tukui support

--- curse.sh
+++ curse.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]
+  then
+    echo "Usage: curse output_file"
+    exit 1
+fi
+
 index=0
 page_size=500
 number_of_addons=$page_size
@@ -10,8 +16,10 @@ do
   data=$(curl -s $curse_endpoint)
   number_of_addons=$(echo "$data" | jq '. | length')
   echo $(echo "$data" | jq 'map({"id": .id, "name": .name, "summary": .summary, "numberOfDownloads": .downloadCount, "categories": [.categories[] | .name], "source": "curse" })') > new.json
-  addons=$(echo $addons | jq -c --argfile n new.json '. + $n')
+  addons=$(echo $addons | jq -r -c --argfile n new.json '. + $n')
   rm new.json
   index=$(( $index + $page_size ))
 done
-echo $addons
+
+echo $addons > $1
+
---
+++ tukui.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [ $# -eq 0 ]
+  then
+    echo "Usage: tukui output_file"
+    exit 1
+fi
+tukui_endpoint="https://www.tukui.org/api.php?addons=all"
+data=$(curl -s $tukui_endpoint)
+echo "$data" | jq -c 'map({"id": .id, "name": .name, "summary": .small_desc | gsub("[\\r\\n\\t]"; ""), "numberOfDownloads": .downloads | tonumber, "categories": [.category], "source": "tukui" })' > $1
