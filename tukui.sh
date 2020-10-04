09035ac93d546aa27ba23273e34bb629e3bfcee2
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2020-10-04 12:08:05 +0200

    chore: combine tukui feeds

--- tukui.sh
+++ tukui.sh
@@ -4,6 +4,26 @@ if [ $# -eq 0 ]
     echo "Usage: tukui output_file"
     exit 1
 fi
-tukui_endpoint="https://www.tukui.org/api.php?addons=all"
-data=$(curl -s $tukui_endpoint)
-echo "$data" | jq -c 'map({"id": .id | tonumber, "name": .name, "summary": .small_desc | gsub("[\\r\\n\\t]"; ""), "numberOfDownloads": .downloads | tonumber, "categories": [.category], "source": "tukui" })' > $1
+
+endpoint="https://www.tukui.org/api.php"
+retail_endpoint="$endpoint?addons=all"
+classic_endpoint="$endpoint?classic-addons=all"
+tmp=$(mktemp -d -t ci-XXXXXXXXXX)
+retail=$tmp/retail.json
+classic=$tmp/classic.json
+all=$tmp/all.json
+curl -s $retail_endpoint | jq '[.[] | . + { "flavors": ["wow_retail"] }]' > $retail
+curl -s $classic_endpoint | jq '[.[] | . + { "flavors": ["wow_classic"] }]' > $classic
+jq -s add $retail $classic > $all
+jq -c \
+  'map(
+  {
+    "id": .id|tonumber,
+    "name": .name,
+    "summary": .small_desc|gsub("[\\r\\n\\t]"; ""),
+    "numberOfDownloads": .downloads|tonumber,
+    "categories": [.category],
+    "flavors": .flavors,
+    "source": "tukui"
+  })' $all > $1
+rm -rf tmp
