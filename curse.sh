940402d0baf7122efbc2450f4a8daa1a4a38d2fe
Peter Gammelgaard Poulsen <peter.gammelgaard@gmail.com>
2020-10-04 13:25:57 +0200

    chore: curse clean up

--- curse.sh
+++ curse.sh
@@ -9,17 +9,30 @@ fi
 index=0
 page_size=500
 number_of_addons=$page_size
-addons=[]
+endpoint="https://addons-ecs.forgesvc.net/api/v2/addon/search?gameId=1&pageSize=${page_size}"
+tmp=$(mktemp -d -t ci-XXXXXXXXXX)
+new=$tmp/new.json
+running=$tmp/running.json
+all=$tmp/addons.json
+touch $running
 while [ $number_of_addons == $page_size ]
 do
-  curse_endpoint="https://addons-ecs.forgesvc.net/api/v2/addon/search?gameId=1&pageSize=${page_size}&index=${index}"
+  curse_endpoint="$endpoint&index=${index}"
   data=$(curl -s $curse_endpoint)
-  number_of_addons=$(echo "$data" | jq '. | length')
-  echo $(echo "$data" | jq 'map({"id": .id, "name": .name, "summary": .summary, "numberOfDownloads": .downloadCount, "categories": [.categories[] | .name], "flavors": [.gameVersionLatestFiles[] | .gameVersionFlavor] | unique, "source": "curse" })') > new.json
-  addons=$(echo $addons | jq -r -c --argfile n new.json '. + $n')
-  rm new.json
+  number_of_addons=$(echo $data | jq '. | length')
+  echo $data | jq \
+    'map({
+      "id": .id,
+      "name": .name,
+      "summary": .summary,
+      "numberOfDownloads": .downloadCount,
+      "categories": [.categories[] | .name],
+      "flavors": [.gameVersionLatestFiles[] | .gameVersionFlavor] | unique,
+      "source": "curse" })' > $new
+  jq -s add $running $new > $all
+  cat $all > $running
   index=$(( $index + $page_size ))
 done
 
-echo $addons > $1
-
+cat $all > $1
+rm -rf $tmp
