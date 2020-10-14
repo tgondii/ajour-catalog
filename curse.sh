ac2cbf62819e72d7c1da9161d4fa03d10a6c9a05
Casper Rogild Storm <casper+github@rogildstorm.com>
2020-10-14 12:17:05 +0200

    feat: added website_url to payload

--- curse.sh
+++ curse.sh
@@ -15,7 +15,7 @@ do
   curse_endpoint="https://addons-ecs.forgesvc.net/api/v2/addon/search?gameId=1&pageSize=${page_size}&index=${index}"
   data=$(curl -s $curse_endpoint)
   number_of_addons=$(echo "$data" | jq '. | length')
-  echo $(echo "$data" | jq 'map({"id": .id, "name": .name, "summary": .summary, "numberOfDownloads": .downloadCount, "categories": [.categories[] | .name], "source": "curse" })') > new.json
+  echo $(echo "$data" | jq 'map({"id": .id, "website_url": .websiteUrl, "name": .name, "summary": .summary, "numberOfDownloads": .downloadCount, "categories": [.categories[] | .name], "source": "curse" })') > new.json
   addons=$(echo $addons | jq -r -c --argfile n new.json '. + $n')
   rm new.json
   index=$(( $index + $page_size ))
